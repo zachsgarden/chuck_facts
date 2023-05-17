750d67936171e9a54f8aae9cbf5ccb7f3b88e958
Zach Kamp <kamp.zach@gmail.com>
2023-05-16 23:57:17 -0400

    api fetch with data displaying

---
+++ script.js
@@ -0,0 +1,19 @@
+fetch("https://api.chucknorris.io/jokes/random")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then((data) => {
+    displayFact(data.value);
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
+
+function displayFact(fact) {
+  const factDiv = document.getElementById("fact-wrap");
+  const factText = document.createElement("h1");
+  factText.innerHTML = fact;
+  factDiv.appendChild(factText);
+}
