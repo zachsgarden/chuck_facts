ede6ddf05028fd7faf62dea6b64c8d248319f534
Zach Kamp <kamp.zach@gmail.com>
2023-05-17 00:05:09 -0400

    button added

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-fetch("https://api.chucknorris.io/jokes/random")
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("NETWORK RESPONSE ERROR");
-    }
-  })
-  .then((data) => {
-    displayFact(data.value);
-  })
-  .catch((error) => console.error("FETCH ERROR:", error));
+async function getAFact() {
+  await fetch("https://api.chucknorris.io/jokes/random")
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("NETWORK RESPONSE ERROR");
+      }
+    })
+    .then((data) => {
+      displayFact(data.value);
+    })
+    .catch((error) => console.error("FETCH ERROR:", error));
+}
 
 function displayFact(fact) {
   const factDiv = document.getElementById("fact-wrap");
-  const factText = document.createElement("h1");
+  const factText = document.createElement("p");
   factText.innerHTML = fact;
   factDiv.appendChild(factText);
 }
